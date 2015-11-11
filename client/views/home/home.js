22d40ad953fb9ea2ee679a43bd2fd61588bc7cd9
Nguyen Xuan Cong <nguyenxuancong@outlook.com>
2015-11-11 17:48:19 +0700

    sua ty xiu

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,19 +1,19 @@
+var Player;
 Template.home.viewmodel({
     videoUrl: '',
     isValidUrl: function () {
-        return (is.not.empty(this.videoUrl()) && is.url(this.videoUrl()))
+        return (is.not.empty(this.videoUrl()) && isUrl(this.videoUrl()))
     },
     getVideo: function () {
         var _videoUrl = this.videoUrl();
         var self = this;
-        if ((is.not.empty(_videoUrl) && is.url(_videoUrl))) {
+        if ((is.not.empty(_videoUrl) && isUrl(_videoUrl))) {
             Meteor.call('_9xbuddy_getMp4', _videoUrl, function (e, r) {
-                console.log(e,r);
                 if(is.array(r) && r.length >= 1) {
                     var s = _.last(r);
                     if(s && _.has(s,'source')){
-                        var player = new Clappr.Player({source: s.source, parentId: "#player"});
-                        self.videoUrl('');
+                        Player = videojs('_player');
+                        Player.src(s.source);
                     }
                 }
             })
