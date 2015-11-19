553c73e7750b9bdcdd89f779c620d35233d22f59
nxpeacock <nguyenxuancong@gmail.com>
2015-11-19 23:44:41 +0700

    begin stream

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -6,10 +6,11 @@ Template.home.viewmodel({
     },
     getVideo: function () {
         var _videoUrl = this.videoUrl();
+        //console.log(_videoUrl);
         var self = this;
         if ((is.not.empty(_videoUrl) && isUrl(_videoUrl))) {
             Meteor.call('_9xbuddy_getMp4', _videoUrl, function (e, r) {
-                console.info(r);
+                //console.info(r);
                 if(is.array(r) && r.length >= 1) {
                     var s = _.last(r);
                     if(s && _.has(s,'source')){
--- client/views/player/videojs.js
+++ client/views/player/videojs.js
@@ -15,8 +15,21 @@ Template.videoJsPlayer.rendered = function(){
             self.player = videojs('_player');
             self.player.src(self.data.source);
             self.player.ready(function(){
+                self.player.disableProgress({
+                    autoDisable: true
+                });
                 self.player.play();
             });
+
+            self.player.one('loadedmetadata', function() {
+                var duration = self.player.duration();
+                console.info(duration);
+            });
+
+            self.player.on('timeupdate',function(){
+                var isPaused = self.player.paused;
+                if(isPaused) self.player.play();
+            })
         }catch(ex){
             console.error(ex);
         }
